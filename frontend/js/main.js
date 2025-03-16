9563c889d9fc427c9bd7b138b3b95afa61cb48c9
Sinan Uyğun <64197926+sinanuygun7@users.noreply.github.com>
2025-03-16 17:51:06 +0300

    card js

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -2,5 +2,12 @@ document.addEventListener("DOMContentLoaded", function() {
     fetch("../../include/navbar.html")
       .then(response => response.text())
       .then(data => document.getElementById("navbar-container").innerHTML = data);
-  });
-  
+});
+document.addEventListener("DOMContentLoaded", function () {
+    fetch("../../include/card.html")
+      .then(response => response.text())
+      .then(data => {
+        document.getElementById("cardsContainer").innerHTML = `<div class="row">${data}</div>`;
+      })
+      .catch(error => console.error("Kartlar yüklenirken hata oluştu:", error));
+});
